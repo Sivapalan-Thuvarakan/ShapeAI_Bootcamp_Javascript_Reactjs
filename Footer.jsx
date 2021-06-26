aab8f2a4ba990ac5364a381695bc62a3c2b68bd1
Sivapalan Thuvarakan <66236630+Sivapalan-Thuvarakan@users.noreply.github.com>
2021-06-26 23:07:44 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer()
+{
+  return(
+    <footer>
+      <p>
+      copyright by ShapeAI and Thuvarakan @ {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
+export default Footer ;
